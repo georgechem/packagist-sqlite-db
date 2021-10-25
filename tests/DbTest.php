2ff965df5dd6e510c94cb44d6fbffca78c5a7d5b
Grzegorz Chrymecki <grzegorzchrymecki@gmail.com>
2021-10-25 21:54:37 +0100

    unit test

---
+++ tests/DbTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Georgechem\SqliteDb\Model;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Db
+ */
+class DbTest extends TestCase
+{
+    private ?\PDO $conn;
+
+    public function setUp():void
+    {
+        $this->conn = Db::getConnection();
+    }
+
+    public function testIsConnectionAvailable()
+    {
+        self::assertInstanceOf(\PDO::class, $this->conn, 'PDO object is available');
+    }
+
+    public function tearDown():void
+    {
+        
+    }
+
+}
